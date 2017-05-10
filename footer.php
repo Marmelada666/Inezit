b482908419ad4db25c06e8a696a16d6ebf01f041
Maro <taziarmarek@gmail.com>
2017-05-10 14:41:30 +0200

    embed google maps

--- footer.php
+++ footer.php
@@ -23,22 +23,25 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-3 col-sm-6 col-xs-12">
-                                    <div class="mailing-widget widget">
-                                        <div class="title-widget">Newsletter</div>
-                                        <div class="content-wiget">
-                                            <p>Sign up for our mailing list to get latest updates and offers.</p>
-                                            <form action="index.html">
-                                                <div class="input-group">
-                                                    <input type="text" placeholder="Email address" class="form-control form-email-widget" /><span class="input-group-btn"><input type="submit" value="✓" class="btn btn-email"/></span></div>
-                                            </form>
-                                            <p>We respect your privacy</p>
-                                            
+                                <div class="col-md-9 col-sm-12">
+                                    <div class="col-md-8 col-md-offset-2">
+                                        <div class="mailing-widget widget">
+                                            <div class="title-widget">Napíšte nám</div>
+                                            <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 3, 'title' => false, 'description' => false ) ); ?>
                                         </div>
                                     </div>
                                 </div>
-                        </div>
+                        </div> <!-- end of row -->
                     </div>
+                </div> <!-- end of container -->
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="map-wrapper">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2629.4738587136253!2d18.617738515485026!3d48.772843114680654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4714dcf1c911d323%3A0x737b64512a616081!2sMgr.+J%C3%A1n+Svitok+-+TOP+Systemsa!5e0!3m2!1ssk!2ssk!4v1494414475697" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>  
+                            </div>
+                        </div> <!-- end of row -->
+                    </div>
+                    <div class="container">
                     <div class="hyperlink">
                         <div class="pull-left hyper-left">
                             <ul class="list-inline">
@@ -50,7 +53,7 @@
                         </div>
                         <div class="pull-right hyper-right">@ SWLABS</div>
                     </div>
-                </div>
+                </div> <!-- end of container -->
             </div>
         </footer>
         <!-- LOADING-->
