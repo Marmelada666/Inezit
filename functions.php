35154ebeed94724b13f198605deec03b2ca27011
Maro <taziarmarek@gmail.com>
2017-05-17 00:26:50 +0200

    narocna oprava nefunkcneho jquery :D

--- functions.php
+++ functions.php
@@ -16,14 +16,14 @@ function inezit_setup()
 add_action('wp_enqueue_scripts','inezit_scripts');
 function inezit_scripts(){
 	// Theme stylesheet.
-	wp_enqueue_style('inezit-style',get_template_directory_uri() . '/assets/css.min/bootstrap.css');
+	wp_enqueue_style('inezit-style',get_template_directory_uri() . '/assets/css/bootstrap.css');
 	wp_enqueue_style('inezit-google-font', 'https://fonts.googleapis.com/css?family=Lato:400,400italic,700,900,300');
 	wp_enqueue_style('inezit-glypher-css', get_template_directory_uri() . '/assets/font/font-icon/font-svg/css/Glyphter.css');
 	wp_enqueue_style('inezit-material-icons-css', 'https://fonts.googleapis.com/icon?family=Material+Icons');
 	wp_enqueue_style('inezit-font-awesome-css', get_template_directory_uri() . '/assets/font/font-icon/font-awesome-4.4.0/css/font-awesome.css');
 	// Scripts
-	wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js',array(),'x.x.x',true);
 	wp_enqueue_script('inezit-jquery-js', get_template_directory_uri() . '/assets/js/jquery/dist/jquery.min.js',array(),'x.x.x',true);
+	wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js',array(),'x.x.x',true);
 	// Lib
 	wp_enqueue_script('inezit-wow', get_template_directory_uri() . '/assets/js/inezit.min.js', array(),'x.x.x',true);
 }
